ec09907825945376409b7f6e16f8750e4c9119bc
ari7hkm <arianhakim90@gmail.com>
2023-08-19 12:17:00 +0330

    Settings Updated

--- todo_list/settings/dev.py
+++ todo_list/settings/dev.py
@@ -1,5 +1,9 @@
+import environ
 from . common import *
 
+
+env = environ.Env()
+environ.Env.read_env()
 
 DEBUG = True
 
@@ -7,7 +11,10 @@ SECRET_KEY = 'django-insecure-3*!n+v=@2z&2a3+q@#9-%&^eigl9wk^^(vl2_omw#p$ha4mr3d
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'HOST': env('DATABASE_HOST'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD')
     }
 }
