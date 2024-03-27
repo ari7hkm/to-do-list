3f58e4d5f697322329d85f52b380e7a80bd37ea3
ari7hkm <arianhakim90@gmail.com>
2024-03-27 15:59:19 +0330

    Changed settings

--- todo_list/settings/dev.py
+++ todo_list/settings/dev.py
@@ -9,12 +9,19 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-3*!n+v=@2z&2a3+q@#9-%&^eigl9wk^^(vl2_omw#p$ha4mr3d'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': env('DATABASE_NAME'),
+#         'HOST': env('DATABASE_HOST'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('DATABASE_PASSWORD')
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_NAME'),
-        'HOST': env('DATABASE_HOST'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD')
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':'db.sqlite3'
     }
 }
